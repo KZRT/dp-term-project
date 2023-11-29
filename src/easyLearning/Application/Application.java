6e97bf0091d725fde547a4d4c7d3fdb15aca94fd
hyun-hyang <jimin3060@naver.com>
2023-11-29 20:06:34 +0900

    GUI : first page

--- src/easyLearning/Application/Application.java
+++ src/easyLearning/Application/Application.java
@@ -1,9 +1,9 @@
-//package easyLearning.Application;
-//
-//import easyLearning.view.gui.MainPage;
-//
-//public class Application {
-//    public static void main(String[] args) {
-//        new MainPage();
-//    }
-//}
+package easyLearning.Application;
+
+import easyLearning.controller.GUI;
+
+public class Application {
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
