ed044d243f99153866802046d7f7a1dcc508c389
hyun-hyang <jimin3060@naver.com>
2023-11-30 04:52:46 +0900

    GUI: step2 finished

--- src/easyLearning/model/GUI/FileTypeFilter.java
+++ src/easyLearning/model/GUI/FileTypeFilter.java
@@ -1,7 +1,12 @@
 package easyLearning.model.GUI;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.*;
 import javax.swing.filechooser.*;
+import javax.swing.table.DefaultTableModel;
 
 public class FileTypeFilter extends FileFilter{
     private final String extension;
@@ -24,4 +29,27 @@ public class FileTypeFilter extends FileFilter{
     public String getDescription() {
         return description + String.format(" (*%s)", extension);
     }
+
+    public static DefaultTableModel createTableFromCSV(String filePath) {
+        DefaultTableModel model = new DefaultTableModel();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            // Read the header
+            String[] headers = br.readLine().split(",");
+            model.setColumnIdentifiers(headers);
+
+            // Read the data
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                model.addRow(data);
+            }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error loading CSV into table: " + e.getMessage());
+        }
+
+        return model;
+    }
+
+
 }
--- src/easyLearning/model/GUI/userSelectFrame.java
+++ src/easyLearning/model/GUI/userSelectFrame.java
@@ -1,12 +1,14 @@
 package easyLearning.model.GUI;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+
+import static easyLearning.model.GUI.FileTypeFilter.createTableFromCSV;
+
 
 public class userSelectFrame extends JFrame{
 
@@ -19,55 +21,55 @@ public class userSelectFrame extends JFrame{
     private JButton submitButton;
     private JComboBox comboBox1;
     private JCheckBox checkBox1;
+    private JScrollPane scrollPane1;
+
+    private File file;
 
     public userSelectFrame() {
-        setContentPane(MainPanel);
+        SwingUtilities.invokeLater(() -> {
+                    setContentPane(MainPanel);
 
-        setTitle("미리보는 AI체험기");
-        setSize(900, 1000);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setBackground(new Color(83,88,76));
+                    setTitle("미리보는 AI체험기");
+                    setSize(900, 1000);
+                    setLocationRelativeTo(null);
+                    //setResizable(false);
+                    setBackground(new Color(83, 88, 76));
 
-        pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+                    pack();
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    setVisible(true);
+                });
 
+        importButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fs = new JFileChooser(new File("c:\\"));
+                fs.setDialogTitle("Open a File");
+                fs.setFileFilter(new FileTypeFilter(".csv", "CSV File"));
+                int result = fs.showOpenDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File file = fs.getSelectedFile();
+                    try {
+                        pleaseInsertFileFormatTextArea.setText(file.getAbsolutePath());
+                        loadCSVIntoTable(file);
 
-        importButton.addActionListener(new Listener());
+                    } catch (Exception exception) {
+                        JOptionPane.showMessageDialog(null, exception.getMessage());
+                    }
+                }
 
+            }
+        });
     }
 
-    class Listener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser fs = new JFileChooser(new File("c:\\"));
-            fs.setDialogTitle("Open a File");
-            fs.setFileFilter(new FileTypeFilter(".txt", "Text File"));
-            fs.setFileFilter(new FileTypeFilter(".dat", "Data File"));
-            fs.setFileFilter(new FileTypeFilter(".hwp", "한글 파일"));
-            int result = fs.showOpenDialog(null);
-            if(result == JFileChooser.APPROVE_OPTION)
-            {
-                File file = fs.getSelectedFile();
-                try
-                {
-                    BufferedReader br = new BufferedReader(new FileReader(file.getPath()));
-                    String line = "";
-                    String s = "";
-                    while((line = br.readLine()) != null)
-                    {
-                        s += line + "\n";
-                    }
-                    pleaseInsertFileFormatTextArea.setText(s);
-                    if (br != null)
-                        br.close();
-                }
-                catch (Exception exception)
-                {
-                    JOptionPane.showMessageDialog(null, exception.getMessage());
-                }
-            }
+    private void loadCSVIntoTable(File csvFile) {
+        try {
+            // Read CSV file and create a DefaultTableModel
+            DefaultTableModel model = createTableFromCSV(csvFile.getAbsolutePath());
+            this.table1.setModel(model);
+            this.table1.repaint();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error loading CSV into table: " + e.getMessage());
         }
     }
 }
