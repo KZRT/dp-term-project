e0466ff9d82ed7c5f7ab7f5de5ac9bced6406abf
gemutsruhe <gemutsruhe@cau.ac.kr>
2023-11-30 19:13:20 +0900

    feat: DatasetExporter

--- src/com/holub/database/DatasetExporter.java
+++ src/com/holub/database/DatasetExporter.java
@@ -1,2 +1,43 @@
-package com.holub.database;public class DatasetExporter {
+package com.holub.database;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+
+public class DatasetExporter implements Table.Exporter {
+
+    private final Writer out;
+    private int width;
+
+    private Iterator columnNames;
+
+    boolean columnLine = true;
+
+    public DatasetExporter(Writer out) {
+        this.out = out;
+    }
+
+    public void storeMetadata(String tableName, int width, int height, Iterator columnNames) throws IOException {
+        this.width = width;
+        this.columnNames = columnNames;
+        storeRow(columnNames); // comma separated list of columns ids
+    }
+
+    public void storeRow(Iterator data) throws IOException {
+        int i = width;
+
+        while (data.hasNext()) {
+            Object datum = data.next();
+
+            if (datum != null && columnLine != true) out.write(datum.toString());
+
+            if (--i > 0) out.write(",\t");
+        }
+        if(columnLine == true) columnLine = false;
+        out.write("\n");
+    }
+
+    public void startTable() throws IOException {/*nothing to do*/}
+
+    public void endTable() throws IOException {/*nothing to do*/}
 }
