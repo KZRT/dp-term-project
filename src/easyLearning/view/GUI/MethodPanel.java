5674241157f7d6977a5d366df92f9382aeb6b873
hyun-hyang <jimin3060@naver.com>
2023-11-30 17:29:58 +0900

    GUI: listener added at checkboxlist

--- src/easyLearning/view/GUI/MethodPanel.java
+++ src/easyLearning/view/GUI/MethodPanel.java
@@ -2,16 +2,20 @@ package easyLearning.view.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MethodPanel extends JPanel {
 
     List<JCheckBox> checkBoxList;
     Color color1;
+
 
     public MethodPanel(){
 
@@ -34,6 +38,7 @@ public class MethodPanel extends JPanel {
             String line;
             while ((line = br.readLine()) != null) {
                 JCheckBox checkBox = new JCheckBox(line);
+                checkBox.addActionListener(new MethodListener());
                 checkBoxList.add(checkBox);
             }
         } catch (IOException e) {
@@ -45,3 +50,24 @@ public class MethodPanel extends JPanel {
 
 
 }
+
+class MethodListener implements ActionListener {
+    private static List<String> selectedItems = new ArrayList<>();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JCheckBox source = (JCheckBox) e.getSource();
+
+        if (source.isSelected()) {
+            // 체크박스가 선택되었을 때
+            selectedItems.add(source.getText());
+        } else {
+            // 체크박스가 해제되었을 때
+            selectedItems.remove(source.getText());
+        }
+
+        // 선택된 항목들 출력
+        System.out.println("Selected Items: " + selectedItems);
+    }
+};
+
+
